49d1b1c7371d1bd46c1d576db26be03f2ceccc2d
KadryDAV <alialkadryofficial@gmail.com>
2024-02-14 15:44:54 -0800

    completed assignment

---
+++ es2015.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+
+// Answer 1
+//You can reassign and declare with var but only reassign with let
+
+// Answer 2
+// with const you cannot redeclare or reassign with const
+
+// Answer 3
+// You can reassign with let but cannot reasssign or declare with const
+
+// Answer 4
+// It’s the way that we explain how the JS compiler works. Variables are lifting or “hoisted” to the top of the scope they are declared in.
